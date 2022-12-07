b937aa1fce0d804d9dd4219d674096b44b060206
Liplan Lekipising <leeplanke@gmail.com>
2022-12-07 16:44:26 +0200

    initialize app

---
+++ main.py
@@ -0,0 +1,42 @@
+from imageai.Detection import VideoObjectDetection
+import json
+from flask import (
+    Flask,
+    request
+)
+import os
+execution_path = os.getcwd()
+detector = VideoObjectDetection()
+detector.setModelTypeAsTinyYOLOv3()
+detector.setModelPath(os.path.join(
+    execution_path, "yolo-tiny.h5"))
+detector.loadModel()
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # this method receives form data from the client which is video file
+    # and returns the predicted class
+
+    # get the file from the request
+    file = request.files['file']
+    # save the file to disk
+    file.save('video.mp4')
+    video_path = detector.detectObjectsFromVideo(input_file_path=os.path.join(execution_path, "video.mp4"),
+                                                 output_file_path=os.path.join(execution_path, "traffic_detected"), frames_per_second=20, log_progress=True)
+    print(video_path)
+    return json.dumps(video_path)
+
+
+# defalut route
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Next Word Predictor</h1>"
+
+
+app.run()
