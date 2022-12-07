4309c7ba1c21352bdbaec3ccec8df2a557869efc
Liplan Lekipising <leeplanke@gmail.com>
2022-12-07 20:53:24 +0200

    change endpoint

--- app.py
+++ app.py
@@ -15,19 +15,22 @@ detector.loadModel()
 app = Flask(__name__)
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/detect', methods=['POST'])
 def predict():
     # this method receives form data from the client which is video file
     # and returns the predicted class
 
+
+
     # get the file from the request
-    file = request.files['file']
+    # file = request.files['file']
     # save the file to disk
-    file.save('video.mp4')
-    video_path = detector.detectObjectsFromVideo(input_file_path=os.path.join(execution_path, "sample1.mp4"),
-                                                 output_file_path=os.path.join(execution_path, "traffic_detected"), frames_per_second=20, log_progress=True)
-    print(video_path)
-    return json.dumps(video_path)
+    # file.save('video.mp4')
+    # video_path = detector.detectObjectsFromVideo(input_file_path=os.path.join(execution_path, "sample1.mp4"),
+    #                                              output_file_path=os.path.join(execution_path, "traffic_detected"), frames_per_second=20, log_progress=True)
+    # print(video_path)
+    # send {message: 'success'}
+    return json.dumps({'message': 'success'})
 
 
 # defalut route
